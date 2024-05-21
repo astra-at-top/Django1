7dbccf508ffd584a0e630d78cdc356eba156aa6d
astra-at-top <prateek9574529921@gmail.com>
2024-05-21 08:55:25 +0530

    login

--- student/validation/views.py
+++ student/validation/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 # Create your views here.
 
@@ -22,10 +23,8 @@ class Auths(View):
         if username and email and password and confirmpassword:
             print(username, email, password, confirmpassword,"credintials")
             if password == confirmpassword:
-                User.objects.create_user(username, email, password)
-                return JsonResponse({
-                    "msg": "user created Succesfully"
-                }, status = 200)
+                # User.objects.create_user(username, email, password)
+                return redirect("http://127.0.0.1:8000/auth/login",permanent=True)
             else:
                 return JsonResponse({
                     "msg" : "Password Do not match"
@@ -34,8 +33,24 @@ class Auths(View):
             return JsonResponse({
                 "msg" : "Some field is missing "
             }, status = 500)
+        
 class Login(View):
     template_name = "validation/Loginform.html"
 
     def get(self, request):
         return render(request, self.template_name)
+    
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POSt.get("password")
+
+        user = authenticate(request, username=username,password=password)
+
+        if user is not None:
+            return JsonResponse({
+                "msg" : "User successfully login"
+            })
+        else:
+            return JsonResponse({
+                "msg" : "Fail to login "
+            })
